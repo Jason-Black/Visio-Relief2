1e9bab6ae8a364615b2bc0fd47059aff0677f5fd
jason-black <action.jason.stage.left@gmail.com>
2025-05-14 15:27:01 -0400

    ahh2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jason-black.github.io',
-  base: '/VisioRelief',
+  base: '/Visio-Relief2',
   outDir: './dist',
   build: {
     assets: 'assets'
